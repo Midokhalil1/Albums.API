cca3cfc8bcff6833f08300dee6a0ed39c3a0b3b9
Midokhalil1 <midokhalil22@gmail.com>
2022-11-01 10:09:42 -0400

    built API

---
+++ functions/index.js
@@ -0,0 +1,10 @@
+import functions from 'firebase-functions'
+import express from express()
+import cors from 'cors'
+const app = express()
+app.use(cors())
+app.use(express.json())
+app.get('/albums', getAllAlbums)
+app.post('/albums', createNewAlbums)
+export const api = functions.https.onRequest(app)
+
