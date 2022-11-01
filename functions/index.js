83e10c0c4baab7c25a8044913cc29ce932001bec
Midokhalil1 <midokhalil22@gmail.com>
2022-11-01 10:52:01 -0400

    done

--- functions/index.js
+++ functions/index.js
@@ -1,10 +1,13 @@
-import functions from 'firebase-functions'
-import express from express()
-import cors from 'cors'
-const app = express()
-app.use(cors())
-app.use(express.json())
-app.get('/albums', getAllAlbums)
-app.post('/albums', createNewAlbums)
-export const api = functions.https.onRequest(app)
+import functions from "firebase-functions";
+import express from "express";
+import cors from "cors";
+import { createNewAlbums, getAllAlbums } from "./src/albums.js";
 
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get("/albums", getAllAlbums);
+app.post("/albums", createNewAlbums);
+
+export const api = functions.https.onRequest(app);
